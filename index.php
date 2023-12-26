0855b53b1b7025d82afab33a621640037ddd42ed
Rendy Ariesta Chandra <rendyariesta0@gmail.com>
2023-12-26 21:14:52 +0700

    26/12/2023

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <title>Film - CRUD</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
   <link rel="stylesheet" href="style/style.css" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
 </head>
 
 <body>
@@ -17,31 +18,33 @@
     <main class="container">
       <section>
         <h2 class="text-center judul">Daftar Film</h2>
-        <table class="table mt-5">
+        <table id="myTable" class="display">
           <thead>
             <tr>
-              <th scope="col">No.</th>
-              <th scope="col">Judul</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Tahun Rilis</th>
-              <th scope="col">Aksi</th>
+              <th>No.</th>
+              <th>Judul Film</th>
+              <th>Genre</th>
+              <th>Tahun Rilis</th>
+              <th>Aksi</th>
             </tr>
           </thead>
-
           <tbody>
             <?php
             include 'koneksi.php';
             $no = 1;
-            $query = "SELECT * FROM karyawan";
-            $result = mysqli_query($koneksi, $query);
+            $query = "SELECT Film.*, Genre.nama_genre
+                      FROM Film
+                      INNER JOIN Genre ON Film.id_genre = Genre.id_genre;
+                      ";
+            $result = mysqli_query($conn, $query);
             if ($result && mysqli_num_rows($result) > 0) {
               while ($row = mysqli_fetch_assoc($result)) {
             ?>
                 <tr>
-                  <th scope="row">1</th>
-                  <td>The Batman</td>
-                  <td>Aksi</td>
-                  <td>2022</td>
+                  <td><?php echo $no++; ?></td>
+                  <td><?= $row['judul_film']; ?></td>
+                  <td><?= $row['nama_genre']; ?></td>
+                  <td><?= $row['tahun_rilis']; ?></td>
                   <td>
                     <a href="#">Lihat</a>
                     <a href="#">Edit</a>
@@ -50,7 +53,7 @@
                 </tr>
             <?php      }
             } else {
-              echo '<tr><td colspan="5">Tidak ada data karyawan.</td></tr>';
+              echo '<tr><td colspan="5">Tidak ada data film.</td></tr>';
             } ?>
           </tbody>
         </table>
@@ -73,6 +76,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+  <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
   <!-- end js -->
 </body>
 
--- koneksi.php
+++ koneksi.php
@@ -2,16 +2,12 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$database = "fim_database";
+$database = "film_database";
 
-//membuat koneksi
+// membuat koneksi
 $conn = mysqli_connect($host, $user, $password, $database);
 
-//memeriksa koneksi
+// memeriksa koneksi
 if (!$conn) {
     die("Koneksi Gagal: " . mysqli_connect_error());
 }
-
-echo "koneksi berhasil";
-
-mysqli_close($conn);
