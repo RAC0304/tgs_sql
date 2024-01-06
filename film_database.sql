385ebe31784fc73c38b9a7e3f3a7dce9f981a55d
Rendy Ariesta Chandra <rendyariesta0@gmail.com>
2024-01-06 12:52:53 +0700

    update database

--- film_database.sql
+++ film_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Jan 2024 pada 07.58
+-- Waktu pembuatan: 03 Jan 2024 pada 10.13
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -46,7 +46,29 @@ INSERT INTO `film` (`id_film`, `judul_film`, `gambar_film`, `tahun_rilis`, `dura
 (25, 'Aquaman and the Lost Kingdom II', '659_aquaman2.jpeg', 2023, 124, 3, 'Black Manta berusaha membalas dendam pada Aquaman atas kematian ayahnya. Dengan menggunakan kekuatan Trisula Hitam, dia menjadi musuh yang tangguh. Untuk mempertahankan Atlantis.'),
 (26, 'Be with You (Korean Movie)', '659_be with you (korean movie).jpeg', 2018, 131, 4, 'Woo-Jin (So Ji-Sub) merawat putranya Ji-Ho (Kim Ji-Hwan) sendirian setelah istrinya Soo-A (Son Ye-Jin) meninggal dunia. Sebelum meninggal, ia berjanji akan kembali pada hari hujan satu tahun kemudian. Satu tahun kemudian, Soo-A muncul lagi, tetapi dia tidak ingat apa-apa.'),
 (27, 'All Quiet on the Western Front', '659_All Quiet on the Western Front.jpeg', 2022, 0, 3, 'Pengalaman mengerikan dan penderitaan seorang prajurit muda Jerman di garis depan barat selama Perang Dunia I.'),
-(28, 'Oppenheimer', '659_Oppenheimer.jpeg', 2023, 181, 3, 'Kisah ilmuwan Amerika Serikat, J. Robert Oppenheimer, dan perannya dalam pengembangan bom atom.');
+(28, 'Oppenheimer', '659_Oppenheimer.jpeg', 2023, 181, 3, 'Kisah ilmuwan Amerika Serikat, J. Robert Oppenheimer, dan perannya dalam pengembangan bom atom.'),
+(30, 'Stand By Me Doraemon 2', '659_stand by me doraemon 2.jpeg', 2012, 220, 3, '\"Stand by Me Doraemon 2\" adalah sebuah film animasi Jepang yang merupakan sekuel dari film \"Stand by Me Doraemon\" yang dirilis pada tahun 2014. Film ini mengisahkan petualangan Doraemon dan Nobita di masa depan. Film ini dirilis pada tahun 2020 dan menjadi salah satu film animasi populer di Jepang.');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `genre`
+--
+
+CREATE TABLE `genre` (
+  `id_genre` int(11) NOT NULL,
+  `nama_genre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `genre`
+--
+
+INSERT INTO `genre` (`id_genre`, `nama_genre`) VALUES
+(1, 'Drama'),
+(2, 'Komedi'),
+(3, 'Aksi'),
+(4, 'Romansa');
 
 --
 -- Indexes for dumped tables
@@ -60,6 +82,12 @@ ALTER TABLE `film`
   ADD KEY `id_genre` (`id_genre`);
 
 --
+-- Indeks untuk tabel `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`id_genre`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -67,7 +95,7 @@ ALTER TABLE `film`
 -- AUTO_INCREMENT untuk tabel `film`
 --
 ALTER TABLE `film`
-  MODIFY `id_film` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_film` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
